e8568465279042ca6d2a875e848fd7cf4dc7c9e7
Asei07 <50318074+Asei07@users.noreply.github.com>
2019-11-15 16:15:18 +0900

    Update UserServlet.java

--- mytmall/src/tmall/servlet/UserServlet.java
+++ mytmall/src/tmall/servlet/UserServlet.java
@@ -15,31 +15,31 @@ public class PropertyServlet extends BaseBackServlet {
 	public String list(HttpServletRequest req, HttpServletreqponse resp, Page page) {
   
 		List<User> users = userDao.list(page.getStart(), page.getCount());
-    int total = userDao.getTotal();
-    page.setTotal(total);
+    		int total = userDao.getTotal();
+    		page.setTotal(total);
     
-    req.setAttribute("page",page);
-    req.setAttribute("users",users);
-    return "admin/listUser.jsp";
+    		req.setAttribute("page",page);
+    		req.setAttribute("users",users);
+   		return "admin/listUser.jsp";
 	}
 
 	@Override
 	public String add(HttpServletRequest req, HttpServletreqponse resp, Page page) {
-    return null;
+    		return null;
 	}
 
 	@Override
 	public String delete(HttpServletRequest req, HttpServletreqponse resp, Page page) {
-    return null;
+    		return null;
 	}
 
 	@Override
 	public String edit(HttpServletRequest req, HttpServletreqponse resp, Page page) {
-    return null;
+    		return null;
 	}
 
 	@Override
 	public String update(HttpServletRequest req, HttpServletreqponse resp, Page page) {
-    return null;
+    		return null;
 	}
 }
